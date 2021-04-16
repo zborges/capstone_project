d57af5d92d20684ff7fc7d24b2dc691f8d00bf07
Zack Borges <zackartborges@gmail.com>
2021-04-16 12:59:13 -0400

    fixes update name convention

--- app/controllers/api/gears_controller.rb
+++ app/controllers/api/gears_controller.rb
@@ -12,10 +12,10 @@ class Api::GearsController < ApplicationController
 
   def create
     @gear = Gear.new(
-      item_name: params[:name],
+      item_name: params[:item_name],
       item_description: params[:item_description],
       item_weight: params[:item_weight],
-      item_quantity: params[:quantity],
+      item_quantity: params[:item_quantity],
     )
     @gear.save
     render "show.json.jb"
@@ -24,10 +24,10 @@ class Api::GearsController < ApplicationController
   def update
     input = params[:id]
     @gear = Gear.find(input)
-    @gear.name = params[:item_name] || @gear.item_name
+    @gear.item_name = params[:item_name] || @gear.item_name
     @gear.item_description = params[:item_description] || @gear.item_description
     @gear.item_weight = params[:item_weight] || @gear.item_weight
-    @gear.quantity = params[:item_quantity] || @gear.item_quantity
+    @gear.item_quantity = params[:item_quantity] || @gear.item_quantity
     @gear.item_url = params[:item_url] || @gear.item_url
     if @gear.save
       render "show.json.jb"
